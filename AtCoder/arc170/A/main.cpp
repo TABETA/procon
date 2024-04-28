7638cfa7636513a9766ef556f33ac798ecc32889
ryosuke.abe <rr.abeabe@gmail.com>
2024-04-28 22:04:33 +0900

    Solve arc170_a

---
+++ AtCoder/arc170/A/main.cpp
@@ -0,0 +1,210 @@
+#ifdef _MSVC_LANG
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+// clang-format off
+/* accelration */
+// 高速バイナリ生成
+#ifndef _DEBUG
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+#endif
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define sb(var,l,r) {var[l].pb(r); var[r].pb(l);}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define repr(i, n) for(auto&& i: n)
+
+/* debug */
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline string in_str() {string x; cin >> x; return x;}
+// search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
+}
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+namespace std{
+    template<>
+    class hash<pair<ll,ll>>{
+        public:
+        size_t operator () ( const pair<ll,ll> &p ) const{
+            auto hash1 = hash<ll>{}(p.first);
+            auto hash2 = hash<ll>{}(p.second);
+            return hash1 ^ hash2;
+        }
+    };
+}
+
+
+auto func(long long N, std::string S, std::string T)-> ll{
+    ll l = 0;
+    ll r = 0;
+    if(S == T) return 0;
+    bool canB2A = [&](){
+        for(ll i = 0; i < N; ++i)
+        {
+            if(S[i] == 'B' && T[i] == 'B')
+            {
+                continue;
+            }
+            return T[i] == 'A';
+        }
+        return false;
+    }();
+    bool canA2B = [&](){
+        for(ll i = N-1; i >= 0; --i)
+        {
+            if(S[i] == 'A' && T[i] == 'A')
+            {
+                continue;
+            }
+            return T[i] == 'B';
+        }
+        return false;
+    }();
+    rep(i,N)
+    {
+        if(S[i] == 'B' && T[i] == 'A')
+        {
+            T[i] = '(';
+            ++l;
+        }
+        else if(S[i] == 'A' && T[i] == 'B')
+        {
+            T[i] = ')';
+            ++r;
+        }
+    }
+    ll requiredNumB2A = [&](){
+        vll U(N);
+        ll res = 0;
+        rep(i,N)
+        {
+            ll v = T[i] == '(' ? 1 :
+                    T[i] == ')' ? -1 :
+                    0;
+            ll pre = ((i > 0) ? U[i-1] : 0);
+            U[i] = pre + v;
+            res = min(res, U[i]);
+        }
+        return abs(res);
+    }();
+    if(requiredNumB2A > 0)
+    {
+        if(!canB2A)
+        {
+            return -1;
+        }
+        l += requiredNumB2A;
+    }
+    ll requiredNumA2B = [&](){
+        vll U(N);
+        ll res = 0;
+        repd(i,N)
+        {
+            ll v = T[i] == '(' ? 1 :
+                    T[i] == ')' ? -1 :
+                    0;
+            ll pre = ((i == N-1) ? 0 : U[i+1]);
+            U[i] = pre + v;
+            res = max(res, U[i]);
+        }
+        return res;
+    }();
+    if(requiredNumA2B > 0)
+    {
+        if(!canA2B)
+        {
+            return -1;
+        }
+        r += requiredNumA2B;
+    }
+    if(r == l)
+    {
+        return r;
+    }
+    
+    return -1;
+}
+// clang-format on
+
+int main() {
+    long long N;
+    std::cin >> N;
+    std::string S;
+    std::cin >> S;
+    std::string T;
+    std::cin >> T;
+    auto ans = func(N, S, T);
+    cout << ans << endl;
+    return 0;
+}
