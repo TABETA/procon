eaf4198d2096b98f4bc24700af96bff7b749bd7b
r-abe <rr.abeabe@gmail.com>
2024-05-14 07:26:40 +0900

    Solve abc142/D

--- AtCoder/abc142/D/main.cpp
+++ AtCoder/abc142/D/main.cpp
@@ -102,9 +102,27 @@ namespace std{
         }
     };
 }
-
+template <typename T>
+vector<pair<T, T> > prime_factorize(T N) {
+    vector<pair<T, T> > res;
+    for (T a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        T ex = 0;
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+        res.push_back({a, ex});
+    }
+    if (N != 1) res.push_back({N, 1});
+    return res;
+}
 
 auto func(long long A, long long B){
+    ll d = gcd(A,B);
+    auto p = prime_factorize(d);
+    ll ans = p.size() + 1;
+    cout << ans << endl;
 
 }
 // clang-format on
