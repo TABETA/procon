485f2684fdbfc055609940974cfd7314fc013a10
r-abe <rr.abeabe@gmail.com>
2024-05-17 19:39:52 +0900

    Solve abc293/D

--- AtCoder/abc293/D/main.cpp
+++ AtCoder/abc293/D/main.cpp
@@ -103,11 +103,89 @@ namespace std{
     };
 }
 
-
-auto func(long long N, long long M, std::vector<long long> A, std::vector<std::string> B, std::vector<long long> C, std::vector<std::string> D){
-
-}
 // clang-format on
+
+template<typename T>
+class UnionFind
+{
+public:
+
+  UnionFind() = default;
+
+  /// @brief Union-Find 木を構築します。
+  /// @param n 要素数
+  explicit UnionFind(size_t n)
+    : m_parentsOrSize(n, -1) {}
+
+  /// @brief 頂点 i の root のインデックスを返します。
+  /// @param i 調べる頂点のインデックス
+  /// @return 頂点 i の root のインデックス
+  T find(T i)
+  {
+    if (m_parentsOrSize[i] < 0)
+    {
+      return i;
+    }
+
+    // 経路圧縮
+    return (m_parentsOrSize[i] = find(m_parentsOrSize[i]));
+  }
+  T groups()
+  {
+    T count = 0;
+    for(auto&& v: m_parentsOrSize){
+        if (v < 0)
+        {
+            count++;
+        }
+    }
+    return count;
+  }
+
+  /// @brief a のグループと b のグループを統合します。
+  /// @param a 一方のインデックス
+  /// @param b 他方のインデックス
+  void merge(T a, T b)
+  {
+    a = find(a);
+    b = find(b);
+
+    if (a != b)
+    {
+      // union by size (小さいほうが子になる）
+      if (-m_parentsOrSize[a] < -m_parentsOrSize[b])
+      {
+        std::swap(a, b);
+      }
+
+      m_parentsOrSize[a] += m_parentsOrSize[b];
+      m_parentsOrSize[b] = a;
+    }
+  }
+
+  /// @brief a と b が同じグループに属すかを返します。
+  /// @param a 一方のインデックス
+  /// @param b 他方のインデックス
+  /// @return a と b が同じグループに属す場合 true, それ以外の場合は false
+  bool isSame(T a, T b)
+  {
+    return (find(a) == find(b));
+  }
+
+  /// @brief i が属するグループの要素数を返します。
+  /// @param i インデックス
+  /// @return i が属するグループの要素数
+  T size(T i)
+  {
+    return -m_parentsOrSize[find(i)];
+  }
+
+private:
+
+  // m_parentsOrSize[i] は i の 親,
+  // ただし root の場合は (-1 * そのグループに属する要素数)
+  std::vector<T> m_parentsOrSize;
+};
 
 int main() {
     long long N;
@@ -118,12 +196,24 @@ int main() {
     std::vector<std::string> B(M);
     std::vector<long long> C(M);
     std::vector<std::string> D(M);
+    auto uf = UnionFind<ll>(N*2);
+    rep(i,N){
+        uf.merge(i+1, i+N+1);
+    }
+    ll X = 0;
     for(int i = 0 ; i < M ; i++){
         std::cin >> A[i];
         std::cin >> B[i];
         std::cin >> C[i];
         std::cin >> D[i];
+        ll a = A[i] + (B[i]=="R" ? 0: N);
+        ll c = C[i] + (D[i]=="R" ? 0: N);
+        if(uf.isSame(a,c)){
+            X++;
+        }
+        uf.merge(a,c);
     }
-    func(N, M, std::move(A), std::move(B), std::move(C), std::move(D));
+    ll Y = uf.groups() - X;
+    cout << X << " " << Y << endl;
     return 0;
 }
