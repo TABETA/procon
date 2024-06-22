5faeee68c8bc0d9ec9cfbe8e953752b1cdfff0da
r-abe <rr.abeabe@gmail.com>
2024-06-22 19:45:51 +0900

    Solve abc230/E

--- AtCoder/abc230/E/main.cpp
+++ AtCoder/abc230/E/main.cpp
@@ -111,19 +111,32 @@ namespace std{
     };
 }
 
-
 // clang-format on
-const ll inf = LLONG_MAX;
-
-auto solve(long long N) {
-    ll ans = inf;
-
-    return ans;
+template <typename T>
+vector<pair<T, T> > prime_factorize(T N) {
+    vector<pair<T, T> > res;
+    for (T a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        T ex = 0;
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+        res.push_back({a, ex});
+    }
+    if (N != 1) res.push_back({N, 1});
+    return res;
 }
-
 int main() {
     long long N;
     std::cin >> N;
-    cout << solve(N) << endl;
+    ll ans = 0;
+    for(ll i = 1; i <= N;){
+        ll x = N/i;
+        ll ni = N/x + 1;
+        ans += x * (ni- i);
+        i = ni;
+    }
+    cout << ans << endl;
     return 0;
 }
