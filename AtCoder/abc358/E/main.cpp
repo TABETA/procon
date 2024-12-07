2c418da46682175d24c26998ab14e9e64315ec22
r-abe <rr.abeabe@gmail.com>
2024-12-07 16:26:48 +0900

    Solve abc358/E

--- AtCoder/abc358/E/main.cpp
+++ AtCoder/abc358/E/main.cpp
@@ -130,14 +130,62 @@ ostream &operator<<(ostream &os, const mint &v) {
 }
 
 // clang-format on
+struct modinv {
+  int n; vector<mint> d;
+  modinv(): n(2), d({0,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} invs;
+struct modfact {
+  int n; vector<mint> d;
+  modfact(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*n), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} facts;
+struct modfactinv {
+  int n; vector<mint> d;
+  modfactinv(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+  if (n < k || k < 0) return 0;
+  return facts(n)*ifacts(k)*ifacts(n-k);
+}
 int main() {
+    const ll N = 26;
     long long K;
     std::cin >> K;
-    std::vector<long long> C(26);
-    for(int i = 0 ; i < 26 ; i++){
+    vll C(N);
+    for(int i = 0 ; i < N ; i++){
         std::cin >> C[i];
     }
-    ll ans = 0;
+    vector<mint> DP(K+1, 0);
+    DP[0] = 1;
+    rep(i,N){
+        vector<mint> pre(K+1, 0);
+        swap(DP, pre);
+        rep(j,C[i]+1){
+            rep(k,K+1){
+                ll nj = k+j;
+                if(nj > K) break;
+                DP[nj] += pre[k] * comb(nj, j);
+            }
+        }
+    }
+    mint ans = 0;
+    reps(i,1,K+1){
+        ans += DP[i];
+    }
     cout << ans << endl;
     return 0;
 }
