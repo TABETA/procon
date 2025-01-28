11d3ac643d4f16967ae1a308caf772334f83efef
r-abe <rr.abeabe@gmail.com>
2025-01-28 23:01:54 +0900

    Solve abc345/C

--- AtCoder/abc345/C/main.cpp
+++ AtCoder/abc345/C/main.cpp
@@ -120,10 +120,67 @@ namespace std{
 
 
 // clang-format on
+#include <atcoder/all>
+using namespace atcoder;
+using mint = static_modint<1000000007>;
+struct modinv {
+  int n; vector<mint> d;
+  modinv(): n(2), d({0,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} invs;
+struct modfact {
+  int n; vector<mint> d;
+  modfact(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*n), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} facts;
+struct modfactinv {
+  int n; vector<mint> d;
+  modfactinv(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+  if (n < k || k < 0) return 0;
+  return facts(n)*ifacts(k)*ifacts(n-k);
+}
 int main() {
     std::string S;
     std::cin >> S;
+    map<char,ll> mp;
+    repr(c, S) {
+        mp[c]++;
+    }
     ll ans = 0;
-    cout << ans << endl;
+    if(mp.size() == 1){
+        cout << 1 << endl;
+        return 0;
+    }
+    bool hasSame = false;
+    rep(i,26){
+        auto a = mp['a'+i];
+        if(a == 0) continue;
+        reps(j,i,26){
+            if(i == j){
+                if(a >= 2) hasSame = true;
+            } else {
+                auto b = mp['a'+j];
+                if(b == 0) continue;
+                ll now = a * b;
+                ans += now;
+            }
+        }
+    }
+    cout << ans + hasSame << endl;
     return 0;
 }
