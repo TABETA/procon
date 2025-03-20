0133aacbd61e4b8c4b0abc277c37d08df240d56f
r-abe <rr.abeabe@gmail.com>
2025-03-21 01:25:51 +0900

    Solve abc132/D

--- AtCoder/abc132/D/main.cpp
+++ AtCoder/abc132/D/main.cpp
@@ -130,12 +130,45 @@ ostream &operator<<(ostream &os, const mint &v) {
 }
 
 // clang-format on
+struct modinv {
+  int n; vector<mint> d;
+  modinv(): n(2), d({0,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(-d[mint::mod()%n]*(mint::mod()/n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} invs;
+struct modfact {
+  int n; vector<mint> d;
+  modfact(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*n), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} facts;
+struct modfactinv {
+  int n; vector<mint> d;
+  modfactinv(): n(2), d({1,1}) {}
+  mint operator()(int i) {
+    while (n <= i) d.push_back(d.back()*invs(n)), ++n;
+    return d[i];
+  }
+  mint operator[](int i) const { return d[i];}
+} ifacts;
+mint comb(int n, int k) {
+  if (n < k || k < 0) return 0;
+  return facts(n)*ifacts(k)*ifacts(n-k);
+}
 int main() {
     long long N;
     std::cin >> N;
     long long K;
     std::cin >> K;
-    ll ans = 0;
-    cout << ans << endl;
+    rep(i,K){
+        mint ans = comb(N-K+1,i+1) * comb(K-1,i);
+        cout << ans << '\n';
+    }
     return 0;
 }
