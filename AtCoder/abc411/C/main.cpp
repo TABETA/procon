b5cd2f9a0d2eb5f08cac174ffb2def4a82f75841
r-abe <rr.abeabe@gmail.com>
2025-06-21 22:40:50 +0900

    Add abc411 results

---
+++ AtCoder/abc411/C/main.cpp
@@ -0,0 +1,188 @@
+#ifdef _MSVC_LANG
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+// clang-format off
+/* accelration */
+// 高速バイナリ生成
+#ifndef _DEBUG
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+#endif
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+const int inf = 1001001001;
+const ll linf = 1001001001001001001ll;
+
+/* define short */
+#define CIN(type, name) type name; cin >> name;
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define repr(i, n) for(auto&& i: n)
+
+/* debug */
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+
+/* func */
+// search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
+}
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+    for (int i = 0; i < (int)v.size(); i++) {
+        os << v[i] << (i + 1 != (int)v.size() ? " " : "");
+    }
+    return os;
+}
+template<typename T>
+pair<T,T> operator+(const pair<T,T>& lhs, const pair<T,T> rhs){
+    return pair<T,T>{lhs.first+rhs.first, lhs.second+rhs.second};
+}
+
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+namespace std{
+    template<>
+    class hash<pair<ll,ll>>{
+        public:
+        size_t operator () ( const pair<ll,ll> &p ) const{
+            auto hash1 = hash<ll>{}(p.first);
+            auto hash2 = hash<ll>{}(p.second);
+            return hash1 ^ hash2;
+        }
+    };
+}
+
+
+// clang-format on
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+    using P = pair<ll,ll>;
+    set<P> S;
+    auto add = [&](P p) {
+        ll a = p.first, b = p.second;
+        if(a >= b) return;
+        S.emplace(a,b);
+    };
+    auto bundan  = [&](auto it, ll a) {
+        if(it->first <= a && a < it->second) {
+            // 含まれていた場合はaで分断する
+            P p1 = {it->first, a}; 
+            P p2 = {a+1, it->second};
+            S.erase(it);
+            add(p1);
+            add(p2);
+            return true;
+        }
+        return false;
+    };
+    rep(_, Q) {
+        ll a;
+        cin >> a;
+        P p = {a, a+1};
+        auto it = S.lower_bound(p);
+        bool ok = false;
+        if(it != S.end()) {
+            if(bundan(it, a)) {
+                ok = true;
+            }
+        }
+        if(!ok && it != S.begin()) {
+            auto jt = prev(it);
+            if(bundan(jt, a)){
+                ok = true;
+            }
+        }
+        if(!ok) {
+            S.insert(p);
+            auto j = S.find(p);
+            auto k = next(j);
+            if(k != S.end() && k->first == j->second) {
+                // 右側が隣接している場合は結合
+                auto q = P{j->first, k->second};
+                S.erase(k);
+                S.erase(j);
+                S.insert(q);
+                j = S.find(q);
+            }
+            if(j != S.begin()) {
+                auto i = prev(j);
+                if(i != S.end() && i->second == j->first) {
+                    // 左側が隣接している場合は結合
+                    auto q = P{i->first, j->second};
+                    S.erase(j);
+                    S.erase(i);
+                    S.insert(q);
+                }
+            }
+        }
+        cout << S.size() << endl;
+    }
+    return 0;
+}
