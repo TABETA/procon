7e8da3b0ade800545b4659da8b1c9f8fbbf9b852
r-abe <rr.abeabe@gmail.com>
2025-08-24 22:47:38 +0900

    Add abc420 results

---
+++ AtCoder/abc420/E/main.cpp
@@ -0,0 +1,221 @@
+#ifdef _MSVC_LANG
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+// clang-format off
+/* accelration */
+// 高速バイナリ生成
+#ifndef _DEBUG
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+#endif
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+const int inf = 1001001001;
+const ll linf = 1001001001001001001ll;
+
+/* define short */
+#define CIN(type, name) type name; cin >> name;
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define repr(i, n) for(auto&& i: n)
+
+/* debug */
+#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+
+/* func */
+// search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
+template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
+    auto itr = std::find(vec.begin(), vec.end(), element);
+    size_t index = std::distance( vec.begin(), itr );
+    if (index == vec.size() || index >= search_length) {return false;} else {return true;}
+}
+template <typename T> inline void print(const vector<T>& v, string s = " ")
+    {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p)
+    {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
+    {for (auto&& p : v) print(p);}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+    for (int i = 0; i < (int)v.size(); i++) {
+        os << v[i] << (i + 1 != (int)v.size() ? " " : "");
+    }
+    return os;
+}
+template<typename T>
+pair<T,T> operator+(const pair<T,T>& lhs, const pair<T,T> rhs){
+    return pair<T,T>{lhs.first+rhs.first, lhs.second+rhs.second};
+}
+
+// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+namespace std{
+    template<>
+    class hash<pair<ll,ll>>{
+        public:
+        size_t operator () ( const pair<ll,ll> &p ) const{
+            auto hash1 = hash<ll>{}(p.first);
+            auto hash2 = hash<ll>{}(p.second);
+            return hash1 ^ hash2;
+        }
+    };
+}
+
+const string YES = "Yes";
+const string NO = "No";
+
+// clang-format on
+template <typename T>
+class UnionFind {
+   public:
+    UnionFind() = default;
+
+    /// @brief Union-Find 木を構築します。
+    /// @param n 要素数
+    explicit UnionFind(size_t n)
+        : m_parentsOrSize(n, -1), m_d(n, 0), m_col(n, false) {}
+
+    /// @brief 頂点 i の root のインデックスを返します。
+    /// @param i 調べる頂点のインデックス
+    /// @return 頂点 i の root のインデックス
+    T find(T i) {
+        if (m_parentsOrSize[i] < 0) {
+            return i;
+        }
+
+        // 経路圧縮
+        return (m_parentsOrSize[i] = find(m_parentsOrSize[i]));
+    }
+
+    /// @brief a のグループと b のグループを統合します。
+    /// @param a 一方のインデックス
+    /// @param b 他方のインデックス
+    void merge(T a, T b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            // union by size (小さいほうが子になる）
+            if (-m_parentsOrSize[a] < -m_parentsOrSize[b]) {
+                std::swap(a, b);
+            }
+
+            m_parentsOrSize[a] += m_parentsOrSize[b];
+            m_parentsOrSize[b] = a;
+            m_d[a] += m_d[b];
+            m_d[b] = 0;
+        }
+    }
+
+    /// @brief a と b が同じグループに属すかを返します。
+    /// @param a 一方のインデックス
+    /// @param b 他方のインデックス
+    /// @return a と b が同じグループに属す場合 true, それ以外の場合は false
+    bool isSame(T a, T b) { return (find(a) == find(b)); }
+
+    /// @brief i が属するグループの要素数を返します。
+    /// @param i インデックス
+    /// @return i が属するグループの要素数
+    T size(T i) { return -m_parentsOrSize[find(i)]; }
+    void flip(int i) {
+        m_col[i] = !m_col[i];
+        T root = find(i);
+        m_d[root] += m_col[i] ? 1 : -1;
+    }
+    bool isBlack(int i) {
+        T root = find(i);
+        return m_d[root] > 0;
+    }
+
+   private:
+    // m_parentsOrSize[i] は i の 親,
+    // ただし root の場合は (-1 * そのグループに属する要素数)
+    std::vector<T> m_parentsOrSize;
+    std::vector<T> m_d;
+    std::vector<bool> m_col;
+};
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+    UnionFind<ll> uf(N);
+    rep(_, Q) {
+        ll q;
+        cin >> q;
+        if (q == 1) {
+            ll u, v;
+            cin >> u >> v;
+            --u, --v;
+            uf.merge(u, v);
+        }
+        if (q == 2) {
+            ll v;
+            cin >> v;
+            --v;
+            uf.flip(v);
+        }
+        if (q == 3) {
+            ll v;
+            cin >> v;
+            --v;
+            cout << (uf.isBlack(v) ? YES : NO) << '\n';
+        }
+    }
+    return 0;
+}
